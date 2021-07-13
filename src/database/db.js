2008e8e0b77916e34adf3b82ff88758f70370083
paulo-cds <paulo.pdomingues@hotmail.com>
2021-07-13 08:28:32 -0300

    altera rota

--- src/database/db.js
+++ src/database/db.js
@@ -5,7 +5,7 @@ function connect() {
     mongoose.set('useNewUrlParser', true)
     mongoose.set('useUnifiedTopology', true)
 
-    mongoose.connect('mongodb+srv://trequinhos:nos141212@cluster0.2ryrv.mongodb.net/trequinhos?retryWrites=true&w=majority')  //process.env.MONGODB_URL ||  mongodb+srv://trequinhos:nos141212@cluster0.2ryrv.mongodb.net/Cluster0?retryWrites=true&w=majority -- mongodb://localhost:27017/api-trequinhos?readPreference=primary&appname=MongoDB%20Compass&ssl=false
+    mongoose.connect(process.env.MONGODB_URL)  //process.env.MONGODB_URL ||  mongodb+srv://trequinhos:nos141212@cluster0.2ryrv.mongodb.net/Cluster0?retryWrites=true&w=majority -- mongodb://localhost:27017/api-trequinhos?readPreference=primary&appname=MongoDB%20Compass&ssl=false
 
     const db = mongoose.connection
 
